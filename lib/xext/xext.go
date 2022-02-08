fa590bdfbbaf7b1eb45ec123a9faa41f774438ec
pluveto <i@pluvet.com>
2022-02-08 21:59:49 +0800

    chore: ext warning

--- lib/xext/xext.go
+++ lib/xext/xext.go
@@ -30,7 +30,7 @@ func (e ExtDef) DisplaySimple(prefix, suffix string) {
 	// Gitub Uploader (id: github) v1.0.0 - Description
 	fmt.Print(prefix)
 	fmt.Printf("%-32s", color.CyanString(e.Meta.Name))
-	fmt.Printf("id: %-16s", color.YellowString(e.Meta.Id))
+	fmt.Printf("id: %-20s", color.YellowString(e.Meta.Id))
 	if len(e.Meta.Version) > 0 {
 		fmt.Printf(" v%-8s", e.Meta.Version)
 	}
--- user_scripts/snipaste_upload.cmd
+++
@@ -1,5 +0,0 @@
-set "UPGIT_BIN=C:\repo\upgit\upgit.exe"
-set "SNIPASTE_BIN=C:\app\Snipaste\Snipaste.exe"
-%SNIPASTE_BIN% snip
-%UPGIT_BIN% :clipboard --output-type clipboard --output-format markdown
-pause
